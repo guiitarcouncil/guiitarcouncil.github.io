dc37ef64ec1eafe0285c160693b9be9e4416bcd9
ivedantmistry <ivedantmistry@gmail.com>
2025-01-12 19:18:58 +0530

    new footer

--- frontend/src/modules/Footer.jsx
+++ frontend/src/modules/Footer.jsx
@@ -1,6 +1,12 @@
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaYoutube, FaInstagram } from 'react-icons/fa';
-import './Footer.css';
-import logo from '../assets/logo1.png'
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaYoutube,
+  FaInstagram,
+} from "react-icons/fa";
+import "./Footer.css";
+import logo from "../assets/logo1.png";
 
 function Footer() {
   return (
@@ -9,28 +15,54 @@ function Footer() {
         <div className="footer-left">
           <div className="footer-logos">
             <img src={logo} alt="logo" />
-            
           </div>
-          <p className="footer-description">
-          <p>Event Room, Second Floor, GUIITAR Council,</p>
-          <p>Anviksha - GSFC University,</p>
-          <p>P. O. Fertilizernagar,</p>
-          <p>Vadodara-391750, Gujarat,</p>
-          <p >INDIA</p></p>
+          <div className="footer-description">
+            <p>Event Room, Second Floor,</p>
+            <p>Anviksha - GSFC University,</p>
+            {/* <p>P. O. Fertilizernagar,</p> */}
+            <p>Vadodara-391750, Gujarat</p>
+            {/* <p>INDIA</p> */}
+          </div>
+
           <div className="footer-social-icons">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a
+              href="https://www.facebook.com/gsfcuniversity"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
               <FaFacebookF />
             </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a
+              href="https://x.com/gsfcuniversity"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
               <FaTwitter />
             </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a
+              href="https://www.linkedin.com/company/guiitarcouncil/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
               <FaLinkedinIn />
             </a>
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a
+              href="https://www.youtube.com/c/GSFCUniversityOfficial"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
               <FaYoutube />
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a
+              href="https://www.instagram.com/gsfcuniversity/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
               <FaInstagram />
             </a>
           </div>
@@ -49,34 +81,33 @@ function Footer() {
           <div className="footer-section">
             <h4>Innovation Ecosystem</h4>
             <ul>
-              <li>Government</li>
-              <li>Academia</li>
-              <li>Investors</li>
-              <li>Mentors</li>
-              <li>Value Partners</li>
-              <li>Partnerships</li>
+              <li>lorem</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum dolor.</li>
+              <li>Lorem, ipsum dolor.</li>
+              <li>Lorem, ipsum.</li>
             </ul>
           </div>
           <div className="footer-section">
             <h4>Quick Links</h4>
             <ul>
-              <li>lore</li>
-              <li>Startup </li>
-              <li>Terms and Conditions</li>
-              <li>Privacy and Cookies Policy</li>
-              <li>Past Events</li>
+              <li>lorem</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum dolor.</li>
+              <li>Lorem, ipsum dolor.</li>
+              <li>Lorem, ipsum.</li>
             </ul>
           </div>
         </div>
       </div>
-      
+
       <div className="footer-copyright">
         <p>Guiitar Council © 2025</p>
         <p>Made with ❤ in India</p>
-
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
+ 
