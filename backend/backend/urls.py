871f4a54b95131a25e5b21db07d8cb0cc6c02b9f
ivedantmistry <ivedantmistry@gmail.com>
2025-01-14 22:08:52 +0530

    Testing webhook deployment

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  
     path('deploy/', include('deploy.urls')), 
+     path('', include('deploy.urls')),
 ]
