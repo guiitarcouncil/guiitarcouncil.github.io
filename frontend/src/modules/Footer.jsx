22b9bf61066e84fbac085320d605b241a0837708
ravi <work.ravivyas@gmail.com>
2025-01-14 17:13:17 +0530

    Footer Update

--- frontend/src/modules/Footer.jsx
+++ frontend/src/modules/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import logo from "../assets/logo1.png";
+import logo from "../assets/white_logo.png";
 import 'font-awesome/css/font-awesome.min.css'
 
 function Footer() {
@@ -7,52 +7,55 @@ function Footer() {
      <footer>
     <div className="footer-container">
       <div className="footer-section">
-        <p>© 2025 GUIITAR Council</p>
-        <p>417 Main Street</p>
-        <p>Little Rock, AR 72201</p>
-        <p>501.404.9875</p>
-        <p><a href="mailto:founders@venturecenter.co">founders@venturecenter.co</a></p>
+        <p className="footer-copyright">© 2025 GUIITAR Council</p>
+        <p>Event Room, Second Floor,</p>
+        <p>Anviksha - GSFC University,</p>
+        <p>Vadodara, Gujarat - 391750</p>
+        <p>India</p>
+        {/* <p className="footer-mail"><a href="mailto:guiitar@gsfcuniversity.ac.in">guiitar@gsfcuniversity.ac.in</a></p> */}
       </div>
       <div className="footer-divider"></div>
       <div className="footer-section">
         {/* <h4>ENTREPRENEUR SUPPORT</h4> */}
         <ul>
-          <li>ABOUT US</li>
-          <li>MENTORSHIP</li>
-          <li>LEADERSHIP</li>
-          <li>OUR ASSOCIATIONS</li>
-          <li>CONTACT</li>
+          <li><a href="#">Infrastructure</a></li>
+          <li><a href="#">Company Portfolio</a></li>
+          <li><a href="#">Previous Events</a></li>
+          <li><a href="#">Startups Incubated</a></li>
+          <li><a href="#">Innovate with Us</a></li>
         </ul>
       </div>
       <div className="footer-section">
         {/* <h4>ACCELERATOR PROGRAMS</h4> */}
         <ul>
-          <li>SSIP Program</li>
-          <li>Nodal Program</li>
-          <li>Drone Workshop</li>
-          <li>Active Projects/Startups</li>
-          <li>Student-led Projects</li>
+          <li><a href="#">SSIP Program</a></li>
+          <li><a href="#">Nodal Program</a></li>
+          <li><a href="#">Drone Workshop</a></li>
+          <li><a href="#">Active Projects/Startups</a></li>
+          <li><a href="#">Student-led Projects</a></li>
         </ul>
       </div>
       <div className="footer-section">
         {/* <h4>ABOUT THE VC</h4> */}
         <ul>
-          <li>Infrastructure</li>
-          <li>Company Portfolio</li>
-          <li>Previous Events</li>
-          <li>Startups Incubated</li>
-          <li>Innovate with Us</li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Mentorship</a></li>
+          <li><a href="#">Leadership</a></li>
+          <li><a href="#">Our Associations</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </div>
     </div>
     <div className="footer-bottom">
-      <img src={logo} alt="The Venture Center Logo" className="footer-logo" />
+      <img src={logo} alt="GUIITAR Council Logo" className="footer-logo" />
       <div className="social-links">
-        <a href="#" className="social-icon"><i className="fa fa-linkedin"></i></a>
-        <a href="#" className="social-icon"><i className="fa fa-facebook"></i></a>
-        <a href="#" className="social-icon"><i className="fa fa-instagram"></i></a>
-        <a href="#" className="social-icon"><i className="fa fa-twitter"></i></a>
-        <a href="#" className="social-icon"><i className="fa fa-envelope"></i></a>
+        <a href="https://in.linkedin.com/school/gsfcuniversity/" className="social-icon"><i className="fa fa-linkedin"></i></a>
+        <a href="https://api.whatsapp.com/send?phone=917573002444" className="social-icon"><i className="fa fa-whatsapp"></i></a>
+        <a href="https://www.facebook.com/gsfcuniversity" className="social-icon"><i className="fa fa-facebook"></i></a>
+        <a href="https://www.instagram.com/gsfcuniversity" className="social-icon"><i className="fa fa-instagram"></i></a>
+        <a href="https://x.com/GSFCUniversity" className="social-icon"><i className="fa fa-twitter"></i></a>
+        <a href="https://www.youtube.com/c/GSFCUniversityOfficial" className="social-icon"><i className="fa fa-youtube"></i></a>
+        <a href="mailto:guiitar@gsfcuniversity.ac.in" className="social-icon"><i className="fa fa-envelope"></i></a>
       </div>
     </div>
   </footer>
