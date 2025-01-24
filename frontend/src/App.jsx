0b4f9cde729c42414830c4d390d4b299f16caeef
ivedantmistry <ivedantmistry@gmail.com>
2025-01-24 15:01:56 +0530

    one

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
             <>
               <Navbar />
               <About />
-            <Infrastructure/>
+              <Infrastructure />
               <Footer />
             </>
           }
