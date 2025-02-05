f78b1d597d10087cf6624716ee57323ff74dac85
ravi <work.ravivyas@gmail.com>
2025-02-05 14:37:21 +0530

    Revert "Footer Update"
    This reverts commit e7e3fc3d0af92373b4efcd84f93051476bd1f191.

--- frontend/src/modules/Footer.jsx
+++ frontend/src/modules/Footer.jsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import logo1 from "../assets/logos/white.png";
 import "font-awesome/css/font-awesome.min.css";
-import FooterMobile from "./FooterMobile"
 
 function Footer() {
   return (
-    <>
     <footer>
       <div className="footer-container">
         <div className="footer-section">
@@ -199,11 +197,6 @@ function Footer() {
         </div>
       </div>
     </footer>
-    <div className="footer-mobile-ft">
-      <FooterMobile />
-    </div>
-    </>
-   
   );
 }
 
