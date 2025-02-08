181118d74c0f866589b9589abd6a8c2c2a5522e6
ivedantmistry <ivedantmistry@gmail.com>
2025-02-08 11:39:42 +0530

    gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Notfound from "./modules/Notfound";
 import Associations from "./modules/Associations";
 import Build from "./modules/Build";
 import ScrollToTop from "./modules/ScrollTop";
-import Login from "./modules/login";
+// import Login from "./modules/login";
 
 function App() {
   return (
