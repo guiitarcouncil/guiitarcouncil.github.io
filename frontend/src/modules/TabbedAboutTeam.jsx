9b8b05450d6b0b9c0781acb5b7809eb50c3ed33b
ivedantmistry <ivedantmistry@gmail.com>
2025-03-22 09:41:17 +0530

    temp mar22

--- frontend/src/modules/TabbedAboutTeam.jsx
+++ frontend/src/modules/TabbedAboutTeam.jsx
@@ -86,7 +86,7 @@ const TabbedAboutTeam = () => {
   ];
 
   const teamData = {
-    team: [ //8 Resumes remaining
+    team: [
       {
         name: "Mr. Bimal Bhayani",
         role: "CEO, GUIITAR",
