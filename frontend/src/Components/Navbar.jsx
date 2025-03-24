495f5b72adba563b4f4c65c1ce3d202194c99c0f
ivedantmistry <ivedantmistry@gmail.com>
2025-03-24 12:01:48 +0530

    bug fix

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo1 from "../Assets/logos/new.png";
+import logo1 from "../../Assets/logos/new.png";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
